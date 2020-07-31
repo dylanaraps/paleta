f63c1f391ef032081bb9c634a480a168c05ef9b7
Dylan Araps <dylan.araps@gmail.com>
2020-07-31 15:34:27 +0300

    more

--- config.h
+++ config.h
@@ -1,21 +1,17 @@
-/* maximum colors to read */
-/* + 3 special colors (bg, fg, cursor) */
-/* + numerical terminal palette (256 by default) */
-#define MAX_PAL 259
+/* numerical terminal palette (0-256 by default) */
+/* 3 reserved for bg, fg and cursor */
+#define MAX_PAL 3 + 256
 
-/* maximum length of color (with \0) */
-#define MAX_COL 7
+/* maximum length of color */
+#define MAX_COL 6
 
-/* maximum length of sequence (with \0) */
-#define MAX_SEQ 18
-
-/* directory containing terminal devices */
-#define PTS_DIR  "/dev/pts"
+/* maximum length of all sequences */
+#define MAX_SEQ 5000
 
 /* file glob to match for directory entries */
-#define PTS_GLOB "[0-9]*"
+#define PTS_GLOB "/dev/pts/[0-9]*"
 
 /* there should be no reason to change these */
-#define FMT_SPE "\033]%d;#%s\033\\"
-#define FMT_NUM "\033]4;%d;#%s\033\\"
-#define FMT_708 "\033]708;#%s\033\\"
+#define FMT_SPE "\033]%d;#%s\033\\\\"
+#define FMT_NUM "\033]4;%d;#%s\033\\\\"
+#define FMT_708 "\033]708;#%s\033\\\\"
--- paleta.c
+++ paleta.c
@@ -1,26 +1,26 @@
 #define _POSIX_C_SOURCE 200809L
-#include <dirent.h>
-#include <fnmatch.h>
+#include <glob.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
 #include "config.h"
 
-static int   pal_read(void);
-static char *pal_morph(const int max_cols);
-static void  pal_write(char *seq);
+static int  pal_read(void);
+static void pal_morph(const int max_cols);
+static void pal_write(void);
 
-static char palette[MAX_PAL][MAX_COL];
+static char pal[MAX_PAL][MAX_COL + 1];
+static char seq[MAX_SEQ + 1]; /* todo lol */
 
 static int pal_read() {
     char *line = 0;
     int i = 0;
-    size_t size;
-    int ret;
 
     while ((getline(&line, &(size_t){0}, stdin) != -1)) {
+        size_t size;
+        int ret;
+
         if (line[0] == '#') {
             ++line;
         }
@@ -28,19 +28,19 @@ static int pal_read() {
         size = strcspn(line, "\n");
         line[size] = 0;
 
-        if (size > MAX_COL) {
+        if (size > MAX_COL || size == 0) {
             printf("invalid input found on stdin (line: %d)\n", i + 1);
             exit(1);
         }
 
-        ret = snprintf(palette[i], MAX_COL, "%s", line);
+        ret = snprintf(pal[i], MAX_COL + 1, "%s", line);
 
         if (ret < 0) {
             printf("failed to read from stdin\n");
             exit(1);
         }
 
-        if (ret + 1 < MAX_COL) {
+        if (ret < MAX_COL) {
             printf("invalid input found on stdin (line: %d)\n", i + 1);
             exit(1);
         }
@@ -54,114 +54,93 @@ static int pal_read() {
 
     free(line);
 
-    if (i == 0) {
-        printf("error: Failed to read from stdin\n");
-        exit(1);
-    }
-
     printf("bg = %s, fg = %s, \033[7m \033[m = %s\n",
-        palette[0], palette[1], palette[2]);
+        pal[0], pal[1], pal[2]);
     printf("colors %d/%d\n", i - 3, MAX_PAL - 3);
 
     return i;
 }
 
-static char *pal_morph(const int max_cols) {
-    char *seq;
+static void pal_morph(const int max_cols) {
     int ret;
 
-    /* + 1 is extra 708 sequence */
-    seq = calloc(MAX_SEQ * (max_cols + 1), 1);
-
-    if (!seq) {
-        printf("failed to allocate memory\n");
-        exit(1);
-    }
-
-    ret = snprintf(seq, MAX_SEQ, FMT_708, palette[0]);
+    ret = snprintf(NULL, 0, FMT_708, pal[0]);
 
     if (ret < 0) {
         printf("failed to construct sequences\n");
         exit(1);
     }
 
+    ret = snprintf(seq, ret, FMT_708, pal[0]);
+
     for (int i = 0; i < max_cols; i++) {
-        ret = snprintf(
-                seq + strlen(seq),
-                MAX_SEQ,
-                i < 3 ? FMT_SPE : FMT_NUM,
-                i < 3 ? i + 10 : i - 3,
-                palette[i]
-              );
+        size_t seq_l;
+        char *fmt = i < 3 ? FMT_SPE : FMT_NUM;
+        int   off = i < 3 ? i + 10  : i - 3;
+
+        ret = snprintf(NULL, 0, fmt, off, pal[i]);
 
         if (ret < 0) {
             printf("failed to construct sequences\n");
             exit(1);
         }
-    }
 
-    return seq;
-}
+        seq_l = strlen(seq);
 
-static void pal_write(char *seq) {
-    struct dirent *dp;
-    DIR *dir;
-    FILE *file;
-
-    dir = opendir(PTS_DIR);
-
-    if (!dir) {
-        printf("failed to open %s.\n", PTS_DIR);
-        exit(1);
-    }
-
-    if (chdir(PTS_DIR) == -1) {
-        closedir(dir);
-        printf("failed to open %s.\n", PTS_DIR);
-        exit(1);
-    }
-
-    while ((dp = readdir(dir)) != NULL) {
-        if (fnmatch("[0-9]*", dp->d_name, 0) != 0) {
-            continue;
+        if (seq_l + ret > MAX_SEQ + 1) {
+            printf("sequences exceed maximum\n");
+            exit(1);
         }
 
-        file = fopen(dp->d_name, "w");
+        ret = snprintf(seq + seq_l, ret, fmt, off, pal[i]);
+    }
+
+    seq[MAX_SEQ] = 0;
+}
+
+static void pal_write() {
+    glob_t buf;
+
+    glob(PTS_GLOB, GLOB_NOSORT, NULL, &buf);
+
+    for (size_t i = 0; i < buf.gl_pathc; i++) {
+        FILE *file = fopen(buf.gl_pathv[i], "w");
 
         if (file) {
             fprintf(file, "%s", seq);
             fclose(file);
+
+            printf("sent output to %s\n", buf.gl_pathv[i]);
         }
     }
 
-    free(seq);
-    closedir(dir);
+    globfree(&buf);
 }
 
 int main(int argc, char **argv) {
     int ret = 0;
-    char *seq;
 
     if (argc > 1 && *++argv[1]) {
         ret = argv[1][0];
     }
 
     switch (ret) {
-        case 'h':
+        case 'v':
+            printf("paleta 0.1.0\n");
+            break;
+
+        case 0:
+            ret = pal_read();
+            pal_morph(ret);
+            pal_write();
+            break;
+
+        default:
             printf("usage: paleta -[hv] <stdin>\n\n");
             printf("send [#]ffffff\\n over <stdin>.\n");
             printf("- lines 1-3   = bg, fg, and cursor.\n");
             printf("- lines 4-%d = palette (0-%d).\n", MAX_PAL, MAX_PAL - 3);
             break;
-
-        case 'v':
-            printf("paleta 0.1.0\n");
-            break;
-
-        default:
-            ret = pal_read();
-            seq = pal_morph(ret);
-            pal_write(seq);
     }
 
     return 0;
