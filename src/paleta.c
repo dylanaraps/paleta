4201b76c408a4ff6d4d9213d34060e0d89350dcb
Dylan Araps <dylan.araps@gmail.com>
2020-08-01 18:49:24 +0300

    tada

--- src/paleta.c
+++ src/paleta.c
@@ -6,13 +6,99 @@
 #include "config.h"
 #include "log.h"
 
-static char pal[MAX_PAL + 1][MAX_COL + 1];
-
 struct buf {
     size_t size;
     size_t cap;
     char *str;
 };
+
+static char pal[MAX_PAL + 1][MAX_COL + 1];
+
+static void pal_read(void);
+static void pal_morph(const int);
+static void pal_write(const char *);
+
+static void seq_add(struct buf *, const char *, const int, const char *);
+
+static void pal_read() {
+    int c;
+    int color = 0;
+    int num = 0;
+
+    while ((c = fgetc(stdin)) != EOF) {
+        if ((c >= 'A' && c <= 'F') ||
+            (c >= 'a' && c <= 'f') ||
+            (c >= '0' && c <= '9')) {
+
+            if (color > MAX_COL) {
+                die("invalid input found on stdin");
+            }
+
+            pal[num][color]   = c;
+            pal[num][MAX_COL] = 0;
+            color++;
+
+        } else if (c == '\n') {
+            if (color < MAX_COL) {
+                die("invalid input found on stdin");
+            }
+
+            color = 0;
+            num++;
+
+            if (num > MAX_PAL) {
+                break;
+            }
+
+        } else if (c == '#') {
+            continue;
+
+        } else {
+            die("invalid input found on stdin");
+        }
+    }
+
+    pal_morph(num);
+}
+
+static void pal_morph(const int max_cols) {
+    struct buf seq = {
+        .cap = 18, /* most frequent size */
+    };
+
+    const char *fmt_spe = "\033]%d;#%s\033\\";
+    const char *fmt_pal = "\033]4;%d;#%s\033\\";
+
+    /* border background */
+    seq_add(&seq, "\033]%d;#%s\033\\", 708, pal[1]);
+
+    /* cursor */
+    seq_add(&seq, fmt_spe, 12, pal[2]);
+
+    /* numerical palette */
+    for (int i = 3; i < max_cols; i++) {
+        seq_add(&seq, fmt_pal, i - 3, pal[i]);
+
+        /* some terminals require that these sequences go
+         * after colors 0-16. other terminals flicker if
+         * these sequences are sent too late. send them
+         * as soon as possible */
+        switch (i - 3) {
+            case 0:
+                /* background */
+                seq_add(&seq, fmt_spe, 11, pal[1]);
+                break;
+
+            case 15:
+                /* foreground */
+                seq_add(&seq, fmt_spe, 10, pal[0]);
+                break;
+        }
+    }
+
+    pal_write(seq.str);
+    free(seq.str);
+}
 
 static void pal_write(const char *str) {
     glob_t buf;
@@ -59,86 +145,6 @@ static void seq_add(struct buf *seq, const char *fmt,
     }
 
     seq->size += ret;
-}
-
-static void pal_morph(const int max_cols) {
-    struct buf seq = {
-        .cap = 18, /* most frequent size */
-    };
-
-    const char *fmt_spe = "\033]%d;#%s\033\\";
-    const char *fmt_pal = "\033]4;%d;#%s\033\\";
-
-    /* border background */
-    seq_add(&seq, "\033]%d;#%s\033\\", 708, pal[1]);
-
-    /* cursor */
-    seq_add(&seq, fmt_spe, 12, pal[2]);
-
-    /* numerical palette */
-    for (int i = 3; i < max_cols; i++) {
-        seq_add(&seq, fmt_pal, i - 3, pal[i]);
-
-        /* some terminals require that these sequences go
-         * after colors 0-16. other terminals flicker if
-         * these sequences are sent too late. send them
-         * as soon as possible */
-        switch (i - 3) {
-            case 0:
-                /* background */
-                seq_add(&seq, fmt_spe, 11, pal[1]);
-                break;
-
-            case 15:
-                /* foreground */
-                seq_add(&seq, fmt_spe, 10, pal[0]);
-                break;
-        }
-    }
-
-    pal_write(seq.str);
-    free(seq.str);
-}
-
-static void pal_read(void) {
-    int c;
-    int color = 0;
-    int num = 0;
-
-    while ((c = fgetc(stdin)) != EOF) {
-        if ((c >= 'A' && c <= 'F') ||
-            (c >= 'a' && c <= 'f') ||
-            (c >= '0' && c <= '9')) {
-
-            if (color > MAX_COL) {
-                die("invalid input found on stdin");
-            }
-
-            pal[num][color]   = c;
-            pal[num][MAX_COL] = 0;
-            color++;
-
-        } else if (c == '\n') {
-            if (color < MAX_COL) {
-                die("invalid input found on stdin");
-            }
-
-            color = 0;
-            num++;
-
-            if (num > MAX_PAL) {
-                break;
-            }
-
-        } else if (c == '#') {
-            continue;
-
-        } else {
-            die("invalid input found on stdin");
-        }
-    }
-
-    pal_morph(num);
 }
 
 int main(int argc, char **argv) {
